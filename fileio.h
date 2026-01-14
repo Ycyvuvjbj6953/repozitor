eab7bfeac4903e0efb420057306ce74c8a4c1ce8
Ycyvuvjbj6953 <darazuravleva880@gmail.com>
2026-01-14 16:46:19 +0500

    Create fileio.h

---
+++ fileio.h
@@ -0,0 +1,13 @@
+#ifndef FILEIO_H
+#define FILEIO_H
+
+#include <vector>
+#include <string>
+#include "task.h"
+
+using namespace std;
+
+void coxranenie(const vector<Task>& tasks);
+void zagruzka_zadach_isfile(vector<Task>& tasks);
+
+#endif
